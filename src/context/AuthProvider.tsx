6d2fec9a454dfaa7e227ab6faad0a5e615d9901d
puchakayalaapparao <puchakayalaapparao1@gmail.com>
2026-01-10 22:38:45 +0530

    --staff count updated

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -37,7 +37,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       if (error) throw error;
       setProfile(data);
     } catch (err: any) {
-      console.er  ror("Profile Error:", err.message);
+      console.er("Profile Error:", err.message);
       setProfile(null);
     } finally {
       isFetching.current = false;
